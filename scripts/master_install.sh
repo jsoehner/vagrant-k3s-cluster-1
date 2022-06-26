57c34cc43e9beee5fd3a15fb25ed1aedf11e0cec
Jeff Soehner <jsoehner@the-techy.com>
2022-06-26 06:38:21 -0400

    One master & one node plus LB

--- scripts/master_install.sh
+++ scripts/master_install.sh
@@ -6,6 +6,7 @@ mv /tmp/id_rsa*  /root/.ssh/
 chmod 400 /root/.ssh/id_rsa*
 chown root:root  /root/.ssh/id_rsa*
 
+touch /root/.ssh/authorized_keys
 cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
 chmod 400 /root/.ssh/authorized_keys
 chown root:root /root/.ssh/authorized_keys
@@ -14,7 +15,8 @@ chown root:root /root/.ssh/authorized_keys
 echo "127.0.1.1 $(hostname)" >> /etc/hosts
 
 # Get current IP adress to launch k3S
-current_ip=$(/sbin/ip -o -4 addr list enp0s8 | awk '{print $4}' | cut -d/ -f1)
+#current_ip=$(/sbin/ip -o -4 addr list enp0s8 | awk '{print $4}' | cut -d/ -f1)
+current_ip=$(/sbin/ip -o -4 addr list eth1 | awk '{print $4}' | cut -d/ -f1)
 
 # If we are on first node, launch k3s with cluster-init, else we join the existing cluster
 if [ $(hostname) = "kubemaster1" ]
